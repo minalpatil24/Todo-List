3fc27fbd4edd12ea1e5457d91541e8789b84d1a2
Minal Patil <minalpatil9025@gmail.com>
2025-11-18 22:46:17 +0530

    Initial commit - Todo List Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+// import Button from "./Button";
+import TodoList from "./TodoList.jsx";
+
+function App() {
+  return (
+    <>
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
+
